4edce117374004496883be8003a61c4479b4cf60
Clement MASSARD <cmassard@palo-it.com>
2018-07-13 16:05:13 +0200

    Added models and validators

--- server/server.js
+++ server/server.js
@@ -5,28 +5,49 @@ mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
 // var newTodo = new Todo({
-//   text: 'Cook dinner'
+//   text: 'Clean home',
+//   completed: false,
+//   completedAt: new Date()
+// })
+
+// newTodo.save().then((doc) => {
+//   console.log('Saved Todo', doc);
+// }, (err) => {
+//   console.log('Unable to save Todo', err);
 // });
 
-var newTodo2 = new Todo({
-  text: 'Clean home',
-  completed: false,
-  completedAt: new Date()
-})
 
-newTodo2.save().then((doc) => {
-  console.log('Saved Todo', doc);
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3
+  }
+});
+
+var newUser = new User({
+  email: 'email@address.com    '
+});
+
+newUser.save().then((doc) => {
+  console.log(doc);
 }, (err) => {
-  console.log('Unable to save Todo', err);
+  console.log('Unable to save user.', err);
 });
