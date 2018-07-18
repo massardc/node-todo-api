e155d44d4d1abf9ef5f7d31cb4e3a01b03f2071d
Clement MASSARD <cmassard@palo-it.com>
2018-07-18 15:15:21 +0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const data = {
-  id: 44
-};
+const password = 'myPwd';
 
-const token = jwt.sign(data, 'mysecret');
-console.log('token', token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-const decoded = jwt.verify(token, 'mysecret');
-console.log('decoded', decoded);
+const hashedPassword = '$2a$10$dtrzGUvGhDpAZosRXsvufOzSzrqMCAO0aDSyu42TeVbFOAj1El0j.';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+
+// const data = {
+//   id: 44
+// };
+
+// const token = jwt.sign(data, 'mysecret');
+// console.log('token', token);
+
+// const decoded = jwt.verify(token, 'mysecret');
+// console.log('decoded', decoded);
 
 // const message = 'I am Clem';
 // const hash = SHA256(message).toString();
