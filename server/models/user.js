dea35137e6d5e11be944cec143bda80d6ba94c6d
Clement MASSARD <cmassard@palo-it.com>
2018-07-19 11:44:05 +0200

    Added POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -72,6 +72,25 @@ UserSchema.statics.findByToken = function(token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  var User = this;
+   
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, success) => {
+        if (success) {
+          return resolve(user);
+        }
+        return reject();
+      });
+    });
+  });
+};
+
 UserSchema.pre('save', function (next) {
   var user = this;
 
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const { mongoose } = require('./db/mongoose');
 const { Todo } = require( './models/todo');
 const { User } = require('./models/user'); 
 const { authenticate } = require('./middleware/authenticate');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -110,6 +111,38 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   });
+});
+
+// POST /users/login
+app.post('/users/login', (req, res) => {
+  // console.log('req', req);
+  const body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+
+  /* console.log('body', body.email);
+  console.log('body', body.password);
+  User.findOne({email: body.email}).then((user) => {
+    bcrypt.compare(body.password, user.password, (err, success) => {
+      if (success) {
+        console.log('SAME');
+        // res.send(user);
+        return user.generateAuthToken();
+      } else {
+        return res.status(404).send();
+      }
+    });
+  }).catch((e) => {
+    console.log('e', e);
+    res.send(e);
+  }); */
 });
 
 
