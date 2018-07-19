e752c40c4e1f07c64fc31a99c1426706267c0a9d
Clement MASSARD <cmassard@palo-it.com>
2018-07-19 14:04:40 +0200

    Added DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -126,11 +126,19 @@ app.post('/users/login', (req, res) => {
   });
 });
 
-
 // GET /users/me
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
+
+// DELETE /users/me/token
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+})
 
 app.listen(port, () => {
   console.log(`Started up on port ${port}.`);
