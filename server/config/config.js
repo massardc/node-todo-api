5d59f42dfaf1e63a9b7eb3588a88060e8408cd0a
Clement MASSARD <cmassard@palo-it.com>
2018-07-23 14:03:18 +0200

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,11 @@
 const env = process.env.NODE_ENV || 'development';
 console.log('env *****', env);
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-} /* else if (env === 'production') {
-  process.env.MONGODB_URI = 'mongodb://clem:todoapp02@ds155299.mlab.com:55299/todoapp';
-} */
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
+}
